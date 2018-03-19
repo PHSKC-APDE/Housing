8a9d9eb94ebd9d2a9a8b43bb4d12d0caa4f14d85
arokem <arokem@gmail.com>
2018-03-19 14:15:02 -0700

    Consistent caps in RData, formatting.

--- processing/08_pha_analyses_prep.R
+++ processing/08_pha_analyses_prep.R
@@ -20,14 +20,6 @@
 # 2016-08-13, split into separate files 2017-10
 #
 ###############################################################################
-
-rm(list=ls()) #reset
-options(max.print = 9000, tibble.print_max = 50, scipen = 999, width = 100)
-gc()
-#### Set up global parameter and call in libraries ####
-options(max.print = 350, tibble.print_max = 50, scipen = 999)
-housing_path <- "//phdata01/DROF_DATA/DOH DATA/Housing"
-
 library(colorout)
 library(housing)
 library(rgdal)
@@ -37,10 +29,7 @@ library(lubridate) # Used to manipulate dates
 library(tidyverse)
 
 #### Bring in data ####
-# pha_cleanadd_sort_dedup <- readRDS(file = "~/data/Housing/OrganizedData/pha_cleanadd_sort_dedup.Rdata")
-load(file = "~/data/Housing/OrganizedData/pha_cleanadd_sort_dedup.Rdata")
-
-
+load(file = "~/data/Housing/OrganizedData/pha_cleanadd_sort_dedup.RData")
 
 ### Strip out some variables that no longer have meaning
 # (e.g., act_date, act_type, income data, household size (this will need to be remade))
@@ -51,7 +40,8 @@ pha_longitudinal <- pha_cleanadd_sort_dedup %>%
 
 glimpse(pha_longitudinal)
 ### Fix date format
-pha_longitudinal <- pha_longitudinal %>% mutate(dob_m6 = as.Date(dob_m6, origin = "1970-01-01"))
+pha_longitudinal <- pha_longitudinal %>% 
+                        mutate(dob_m6 = as.Date(dob_m6, origin = "1970-01-01"))
 
 ### Fix up remaining program categories (will be moved earlier eventually)
 # Bring in program name mapping table and add new variables
@@ -62,16 +52,22 @@ pha_longitudinal <- pha_longitudinal %>% mutate(dob_m6 = as.Date(dob_m6, origin 
 
 ### Set up time in housing for each row and note when there was a gap in coverage
 pha_longitudinal <- pha_longitudinal %>%
-  mutate(cov_time = interval(start = startdate, end = enddate) / ddays(1) + 1,
-         gap = ifelse(is.na(lag(pid, 1)) | pid != lag(pid, 1), 0,
-                      ifelse(startdate - lag(enddate, 1) > 62, 1, NA))) %>%
-  # Find the number of unique periods a person was in housing
-  group_by(pid, gap) %>%
-  mutate(period = row_number() * gap + 1) %>%
-  ungroup() %>%
-  # Fill in missing data
-  tidyr::fill(., period) %>%
-  select(-gap)
+                        mutate(cov_time = interval(
+                                            start = startdate, 
+                                            end = enddate) / ddays(1) + 1,
+                               gap = ifelse(is.na(lag(pid, 1)) | 
+                                            pid != lag(pid, 1), 0,
+                                            ifelse(startdate - lag(enddate, 1)
+                                                   > 62, 1, NA))
+                                            ) %>%
+                                # Find the number of unique periods a person 
+                                # was in housing
+                                group_by(pid, gap) %>%
+                                  mutate(period = row_number() * gap + 1) %>%
+                                    ungroup() %>%
+                                    # Fill in missing data
+                                    tidyr::fill(., period) %>%
+                                      select(-gap)
 
 
 ### Flag date to use when calculating length of stay
@@ -88,12 +84,16 @@ pha_longitudinal <- pha_longitudinal %>%
 ### Age
 # Age at each date
 pha_longitudinal <- pha_longitudinal %>%
-  mutate(age12 = round(interval(start = dob_m6, end = ymd(20121231)) / years(1), 1),
-         age13 = round(interval(start = dob_m6, end = ymd(20131231)) / years(1), 1),
-         age14 = round(interval(start = dob_m6, end = ymd(20141231)) / years(1), 1),
-         age15 = round(interval(start = dob_m6, end = ymd(20151231)) / years(1), 1),
-         age16 = round(interval(start = dob_m6, end = ymd(20161231)) / years(1), 1)
-  )
+  mutate(age12 = round(interval(start = dob_m6,
+                                end = ymd(20121231)) / years(1), 1),
+         age13 = round(interval(start = dob_m6, 
+                       end = ymd(20131231)) / years(1), 1),
+         age14 = round(interval(start = dob_m6, 
+                       end = ymd(20141231)) / years(1), 1),
+         age15 = round(interval(start = dob_m6, 
+                       end = ymd(20151231)) / years(1), 1),
+         age16 = round(interval(start = dob_m6, 
+                      end = ymd(20161231)) / years(1), 1))
 
 # Single age grouping
 # Note: this is only valid for the person at the time of the start date
@@ -105,8 +105,10 @@ pha_longitudinal <- pha_longitudinal %>%
 
 ### Recode gender and disability
 pha_longitudinal <- pha_longitudinal %>%
-  mutate(gender2 = car::recode(gender_new_m6, "'1' = 'Female'; '2' = 'Male'; else = NA"),
-         disability2 = car::recode(disability, "'1' = 'Disabled'; '0' = 'Not disabled'; else = NA"))
+  mutate(gender2 = car::recode(gender_new_m6, 
+                               "'1' = 'Female'; '2' = 'Male'; else = NA"),
+         disability2 = car::recode(disability, 
+                    "'1' = 'Disabled'; '0' = 'Not disabled'; else = NA"))
 
 ### Subset cases in King County
   # Note: the following zipcodes are from the US census and overlapping King County.
@@ -114,16 +116,28 @@ pha_longitudinal <- pha_longitudinal %>%
   # this method as spatial subsets also leave some points outside, meaning a
   # somewhat unreliable geocoding process.
 
-  zip <- c(98001,98002,98003,98004,98005,98006,98007,98008,98010,98011,98014,98019,98022,98023,98024,98027,98028,98029,98030,98031,98032,98033,98034,98038,98039,98040,98042,98045,98047,98050,98051,98052,98053,98055,98056,98057,98058,98059,98065,98068,98070,98072,98074,98075,98077,98092,98101,98102,98103,98104,98105,98106,98107,98108,98109,98112,98115,98116,98117,98118,98119,98121,98224,98122,98125,98126,98133,98134,98136,98144,98146,98148,98154,98288,98155,98158,98164,98166,98168,98174,98177,98178,98188,98195,98198,98199)
+  zip <- c(98001,98002,98003,98004,98005,98006,98007,98008,98010,98011,98014,
+           98019,98022,98023,98024,98027,98028,98029,98030,98031,98032,98033,
+           98034,98038,98039,98040,98042,98045,98047,98050,98051,98052,98053,
+           98055,98056,98057,98058,98059,98065,98068,98070,98072,98074,98075,
+           98077,98092,98101,98102,98103,98104,98105,98106,98107,98108,98109,
+           98112,98115,98116,98117,98118,98119,98121,98224,98122,98125,98126,
+           98133,98134,98136,98144,98146,98148,98154,98288,98155,98158,98164,
+           98166,98168,98174,98177,98178,98188,98195,98198,98199)
 
   pha_longitudinal_kc <- pha_longitudinal %>%
     filter(unit_zip_new %in% zip) #%>% filter(!is.na(X))
 
 #### Save point ####
-save(pha_longitudinal_kc, file = "~/data/Housing/OrganizedData/pha_longitudinal_kc.Rdata")
-save(pha_longitudinal, file = "~/data/Housing/OrganizedData/pha_longitudinal.Rdata")
+save(pha_longitudinal_kc, 
+     file = "~/data/Housing/OrganizedData/pha_longitudinal_kc.RData")
 
-write.csv(pha_longitudinal_kc, "~/data/Housing/OrganizedData/pha_longitudinal_kc.csv")
+save(pha_longitudinal, 
+    file = "~/data/Housing/OrganizedData/pha_longitudinal.RData")
+
+write.csv(pha_longitudinal_kc, 
+          "~/data/Housing/OrganizedData/pha_longitudinal_kc.csv")
+
 write.csv(pha_longitudinal, "~/data/Housing/OrganizedData/pha_longitudinal.csv")
 
 
