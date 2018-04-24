f0bcc0fddea4b81f75bc703a65ce7980457398a9
Alastair Matheson <alastair.matheson@gmail.com>
2018-04-24 10:42:13 -0700

    Examine demographics of Yesler Terrace residents

---
+++ SHA/YT data/YT_demographic_setup.R
@@ -0,0 +1,142 @@
+###############################################################################
+# Code to join examine demographics of Yesler Terrace residents
+#
+# Alastair Matheson (PHSKC-APDE)
+# alastair.matheson@kingcounty.gov
+# 2017-06-30
+#
+# NOTE THAT THIS CODE IS A WORK IN PROGRESS
+#
+###############################################################################
+
+
+#### Set up global parameter and call in libraries ####
+# Turn scientific notation off and other settings
+options(max.print = 700, scipen = 100, digits = 5)
+
+library(housing) # contains many useful functions for analyses
+library(openxlsx) # Used to import/export Excel files
+library(dplyr) # Used to manipulate data
+
+housing_path <- "//phdata01/DROF_DATA/DOH DATA/Housing"
+
+#### Bring in combined PHA/Medicaid data with some demographics already run ####
+pha_elig_final <- readRDS(file = paste0(housing_path, "/OrganizedData/pha_elig_final.Rda"))
+
+
+#### Set up key variables ####
+### Yesler Terrace and scattered sites indicators
+yt_elig_final <- yesler(pha_elig_final)
+
+### Movements within data
+# Use simplified system for describing movement
+
+# First letter of start_type describes previous address,
+# Second letter of start_type describes current address
+
+# First letter of end_type describes current address,
+# Second letter of end_type describes next address
+
+#   K = KCHA
+#   N = YT address (new unit)
+#   O = non-YT, non-scattered site SHA unit
+#   S = SHA scattered site
+#   U = unknown (i.e., new into SHA system, mostly people who only had Medicaid but not PHA coverage)
+#   Y = YT address (old unit)
+
+
+yt_elig_final <- yt_elig_final %>%
+  arrange(pid2, startdate_c, enddate_c) %>%
+  mutate(
+    # First ID the place for that row
+    place = case_when(
+      is.na(agency_new) ~ "U",
+      agency_new == "KCHA" & !is.na(agency_new) ~ "K",
+      agency_new == "SHA" & !is.na(agency_new) & yt == 0 & ss == 0 ~ "O",
+      agency_new == "SHA" & !is.na(agency_new) & yt_old == 1 ~ "Y",
+      agency_new == "SHA" & !is.na(agency_new) & yt_new == 1 ~ "N",
+      agency_new == "SHA" & !is.na(agency_new) & yt == 0 & ss == 1 ~ "S"
+    ),
+    start_type = NA,
+    start_type = ifelse(pid2 != lag(pid2, 1) | is.na(lag(pid2, 1)), paste0("U", place),
+                        ifelse(pid2 == lag(pid2, 1) & !is.na(lag(pid2, 1)), paste0(lag(place, 1), place), start_type)),
+    end_type = NA,
+    end_type = ifelse((pid2 != lead(pid2, 1) | is.na(lead(pid2, 1))) & enddate_c < as.Date("2017-09-15"), paste0(place, "U"),
+                      ifelse(pid2 == lead(pid2, 1) & !is.na(lead(pid2, 1)), paste0(place, lead(place, 1)), end_type))
+  )
+
+
+### Age
+# Use Medicaid DOB unless missing
+yt_elig_final <- yt_elig_final %>%
+  mutate(dob_c = as.Date(ifelse(is.na(dob_m), dob_h, dob_m), origin = "1970-01-01"))
+
+yt_elig_final <- yt_elig_final %>%
+  mutate(age12 = round(interval(start = dob_c, end = ymd(20121231)) / years(1), 1),
+         age13 = round(interval(start = dob_c, end = ymd(20131231)) / years(1), 1),
+         age14 = round(interval(start = dob_c, end = ymd(20141231)) / years(1), 1),
+         age15 = round(interval(start = dob_c, end = ymd(20151231)) / years(1), 1),
+         age16 = round(interval(start = dob_c, end = ymd(20161231)) / years(1), 1),
+         age17 = round(interval(start = dob_c, end = ymd(20171231)) / years(1), 1)
+  ) %>%
+  # Remove negative ages
+  mutate_at(vars(age12:age17), funs(ifelse(. < 0, 0.01, .)))
+
+# Make groups of ages
+yt_elig_final <- yt_elig_final %>%
+  mutate_at(
+    vars(age12, age13, age14, age15, age16),
+    funs(grp = case_when(
+      . < 18 ~ "<18",
+      between(., 18, 24.99) ~ "18–24",
+      between(., 25, 44.99) ~ "25–44",
+      between(., 45, 61.99) ~ "45–61",
+      between(., 62, 64.99) ~ "62–64",
+      . >= 65 ~ "65+",
+      is.na(.) ~ "Unknown"
+    )
+    )
+  )
+
+
+### Time in housing
+yt_elig_final <- yt_elig_final %>%
+  mutate(length12 = round(interval(start = start_housing, end = ymd(20121231)) / years(1), 1),
+         length13 = round(interval(start = start_housing, end = ymd(20131231)) / years(1), 1),
+         length14 = round(interval(start = start_housing, end = ymd(20141231)) / years(1), 1),
+         length15 = round(interval(start = start_housing, end = ymd(20151231)) / years(1), 1),
+         length16 = round(interval(start = start_housing, end = ymd(20161231)) / years(1), 1),
+         length17 = round(interval(start = start_housing, end = ymd(20171231)) / years(1), 1)
+  ) %>%
+  # Remove negative ages
+  mutate_at(vars(length12:length17), funs(ifelse(. < 0, NA, .)))
+
+# Make groups of time in housing
+yt_elig_final <- yt_elig_final %>%
+  mutate_at(
+    vars(length12, length13, length14, length15, length16),
+    funs(grp = case_when(
+      . < 3 ~ "<3 years",
+      between(., 3, 5.99) ~ "3–<6 years",
+      . >= 6 ~ "6+ years",
+      is.na(.) ~ "Unknown"
+    )
+    )
+  )
+
+
+### Save point
+saveRDS(yt_elig_final, file = paste0(housing_path, "/OrganizedData/SHA cleaning/yt_elig_final.Rds"))
+yt_elig_final <- readRDS(file = paste0(housing_path, "/OrganizedData/SHA cleaning/yt_elig_final.Rds"))
+
+
+
+#### Output to Excel for use in Tableau ####
+# temp <- yt_elig_final
+# # Strip out identifying variables
+# temp <- temp %>% select(pid, gender_new_m6, race2, adult, senior, disability, DUAL_ELIG, COVERAGE_TYPE_IND, agency_new, 
+#                         startdate_h:enddate_o, startdate_c:enroll_type, yt:age16) %>%
+#   arrange(pid, startdate_c, enddate_c)
+# 
+# 
+# write.xlsx(temp, file = "//phdata01/DROF_DATA/DOH DATA/Housing/OrganizedData/Summaries/yt_elig_demogs.xlsx")
