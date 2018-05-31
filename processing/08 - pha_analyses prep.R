421d41d32125dc84adca490f42e285f8058fc30e
Alastair Matheson <alastair.matheson@gmail.com>
2018-05-31 16:21:16 -0700

    Updated for 2017 data. New ZIP source.

--- processing/08 - pha_analyses prep.R
+++ processing/08 - pha_analyses prep.R
@@ -37,14 +37,56 @@ pha_cleanadd_sort_dedup <- readRDS(file = "//phdata01/DROF_DATA/DOH DATA/Housing
 
 
 ### Strip out some variables that no longer have meaning 
-# (e.g., act_date, act_type, income data, household size (this will need to be remade))
+# (e.g., act_date, act_type, household size (this will need to be remade))
 pha_longitudinal <- pha_cleanadd_sort_dedup %>%
-  select(-(hhold_inc_fixed:hhold_inc_vary), -(act_type:correction_date), -(reexam_date:agency), 
-         -(portability:cost_pha), -inc_fixed, -(sha_source), 
-         -(r_white_new_tot:r_hisp_new_tot), -add_num, -drop, -next_hh_act, -add_yr)
+  select(
+    # Original name and SSN variables (with minor clean up)
+    ssn_new, ssn_c, lname_new, fname_new, mname_new, lnamesuf_new, dob,
+    gender_new, citizen, disability, relcode,
+    # New name, SSN, and demog variables (after matching)
+    pid, ssn_id_m6, ssn_id_m6_junk, lname_new_m6, fname_new_m6, mname_new_m6,
+    lnamesuf_new_m6, gender_new_m6, dob_m6, r_aian_new, r_asian_new, r_black_new,
+    r_multi_new, r_nhpi_new, r_white_new, r_hisp_new, race2, age, adult, senior,
+    # Old household info
+    mbr_num, hh_ssn_new:hh_dob, hh_id, 
+    # New household info
+    hh_ssn_id_m6:hh_id_new,
+    # MTW background info
+    list_date, list_zip, list_homeless, housing_act,
+    # Program info
+    agency_new, major_prog, prog_type, subsidy_type, operator_type, 
+    vouch_type_final, agency_prog_concat,
+    # Old address info
+    unit_add:unit_zip,
+    # New address info
+    unit_add_new:unit_zip_new, unit_concat,
+    # Porperty/portfolio info
+    property_id, unit_id, property_name, property_type, portfolio, portfolio_final,
+    # Unit info
+    unit_type, unit_year, access_unit, access_req, access_rec, bed_cnt, move_in_date,
+    # Date info
+    admit_date, startdate, enddate,
+    # Port info
+    port_in, port_out_kcha, port_out_sha, cost_pha,
+    # Personal asset/income info
+    asset_val, asset_inc, inc_fixed, inc_vary, inc, inc_excl, inc_adj,
+    # Household asset/income info
+    hh_asset_val, hh_asset_inc, hh_asset_impute, hh_asset_inc_final,
+    hh_inc_fixed, hh_inc_vary, hh_inc, hh_inc_adj, 
+    hh_inc_tot, hh_inc_deduct, hh_inc_tot_adj,
+    # Rent info
+    rent_type:tb_rent_ceiling,
+    # Received various forms of assistance (SHA only)
+    # dropping for now
+    # Linking and ID variables
+    incasset_id, subsidy_id, vouch_num, cert_id, increment
+    )
 
+
+#### TEMP UNTIL pha_consolidation rerun ####
 ### Fix date format
 pha_longitudinal <- pha_longitudinal %>% mutate(dob_m6 = as.Date(dob_m6, origin = "1970-01-01"))
+#### END TEMP ####
 
 ### Fix up remaining program categories (will be moved earlier eventually)
 # Bring in program name mapping table and add new variables
@@ -56,13 +98,16 @@ pha_longitudinal <- pha_longitudinal %>% mutate(dob_m6 = as.Date(dob_m6, origin 
 ### Set up time in housing for each row and note when there was a gap in coverage
 pha_longitudinal <- pha_longitudinal %>% 
   mutate(cov_time = interval(start = startdate, end = enddate) / ddays(1) + 1,
-         gap = ifelse(is.na(lag(pid, 1)) | pid != lag(pid, 1), 0,
-                      ifelse(startdate - lag(enddate, 1) > 62, 1, NA))) %>%
+         gap = case_when(
+           is.na(lag(pid, 1)) | pid != lag(pid, 1) ~ 0,
+           startdate - lag(enddate, 1) > 62 ~ 1,
+           TRUE ~ NA_real_
+         )) %>%
   # Find the number of unique periods a person was in housing
   group_by(pid, gap) %>%
   mutate(period = row_number() * gap + 1) %>% 
   ungroup() %>%
-  # Fill in missing data
+  # Fill in missing data using previous non-missing value
   tidyr::fill(., period) %>%
   select(-gap)
 
@@ -85,16 +130,20 @@ pha_longitudinal <- pha_longitudinal %>%
          age13 = round(interval(start = dob_m6, end = ymd(20131231)) / years(1), 1),
          age14 = round(interval(start = dob_m6, end = ymd(20141231)) / years(1), 1),
          age15 = round(interval(start = dob_m6, end = ymd(20151231)) / years(1), 1),
-         age16 = round(interval(start = dob_m6, end = ymd(20161231)) / years(1), 1)
+         age16 = round(interval(start = dob_m6, end = ymd(20161231)) / years(1), 1),
+         age17 = round(interval(start = dob_m6, end = ymd(20171231)) / years(1), 1)
   )
 
 # Single age grouping
 # Note: this is only valid for the person at the time of the start date
 # Need to recalculate for each time period being analyzed
 pha_longitudinal <- pha_longitudinal %>%
-  mutate(agegrp = ifelse(adult == 0 & !is.na(adult), "Youth",
-                         ifelse(adult == 1 & senior == 0 & !is.na(adult), "Working age",
-                                ifelse(adult == 1 & senior == 1 & !is.na(adult), "Senior", NA))))
+  mutate(agegrp = case_when(
+    adult == 0 & !is.na(adult) ~ "Youth",
+    adult == 1 & senior == 0 & !is.na(adult) ~ "Working age",
+    adult == 1 & senior == 1 & !is.na(adult) ~ "Senior",
+    TRUE ~ NA_character_
+    ))
 
 ### Recode gender and disability
 pha_longitudinal <- pha_longitudinal %>%
@@ -102,11 +151,16 @@ pha_longitudinal <- pha_longitudinal %>%
          disability2 = car::recode(disability, "'1' = 'Disabled'; '0' = 'Not disabled'; else = NA"))
 
 
-### ZIPs to restrict to KC
-zips <- read.xlsx("//phdata01/DROF_DATA/DOH DATA/Housing/OrganizedData/ZIP filter for KC.xlsx")
+### ZIPs to restrict to KC and surrounds (helps make maps that drop far-flung ports)
+zips <- read.csv(text = RCurl::getURL("https://raw.githubusercontent.com/PHSKC-APDE/reference-data/master/Spatial%20data/KC%20ZIPs.csv"), 
+                 header = TRUE, stringsAsFactors = FALSE)
+names(zips) <- tolower(names(zips))
+zips <- zips %>% select(zipcode) %>% 
+  mutate(kc_area = 1)
+
 pha_longitudinal <- pha_longitudinal %>%
   mutate(unit_zip_new = as.numeric(unit_zip_new)) %>%
-  left_join(., zips, by = c("unit_zip_new" = "zip"))
+  left_join(., zips, by = c("unit_zip_new" = "zipcode"))
 rm(zips)
 
 
