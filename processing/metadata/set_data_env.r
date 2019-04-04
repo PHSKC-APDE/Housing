21810f344eae8ffeb9786ae904c0e08908803d21
Alastair Matheson <alastair.matheson@gmail.com>
2019-04-04 10:42:52 -0700

    Consistent naming for files. Added 2018 options.

--- processing/metadata/set_data_env.r
+++ processing/metadata/set_data_env.r
@@ -1,7 +1,7 @@
 # Function to set data file variable paths and names in working enviroment 
 # inputs: "sha_data", "kcha_data"
 
- set_data_envr = function(METADATA,data_source) {
+ set_data_envr <- function(METADATA,data_source) {
     lapply(seq_along(METADATA[[data_source]]), 
            function(x) {
             assign(names(METADATA[[data_source]][x]), METADATA[[data_source]][[x]], envir=.GlobalEnv)
