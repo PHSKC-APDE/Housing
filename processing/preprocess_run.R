aee5292a6661f6d3e551cf2e15dc0264f737fb64
joseh <joseh@uw.edu>
2019-04-17 16:09:32 +0000

    preprocess run single call

--- processing/preprocess_run.R
+++ processing/preprocess_run.R
@@ -2,22 +2,32 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 
-hild_dir <- args[1]  
+hild_dir <- args[1]
+hild_dir <- "/home/ubuntu/data/HILD/" #temp, will pass through python script
 housing_source <- args[2] 
-local_metadata_path <- args[3]
-UW <- TRUE
-aws <- FALSE
+housing_source <- "/home/joseh/source/Housing/processing/" # temp, will pass through python script
+# local_metadata_path <- args[3]
+
+housing_scripts <- c('01_kcha_sql_load.R',
+                     '02_sha_sql_load.R',
+                     '03_pha_combining.R',
+                     '04_pha_matching.R',
+                     '05_pha_recodes.R',
+                     '06_pha_address_cleaning.R',
+                     '06a_pha_geocoding.R',
+                     '07_pha_consolidation.R',
+                     '08_pha_analyses_prep.R')
 
 start_time <- Sys.time()
-source(paste0(housing_source,'01_kcha_sql_load.R'))
-source(paste0(housing_source,'02_sha_sql_load.R'))
-source(paste0(housing_source,'03_pha_combining.R'))
-source(paste0(housing_source,'04_pha_matching.R'))
-source(paste0(housing_source,'05_pha_recodes.R'))
-source(paste0(housing_source,'06_pha_address_cleaning.R'))
-source(paste0(housing_source,'06a_pha_geocoding.R'))
-source(paste0(housing_source,'07_pha_consolidation.R'))
-source(paste0(housing_source,'08_pha_analyses_prep.R'))
+
+for (rscripts in housing_scripts) {
+  start_time <- Sys.time()
+  source(paste0(housing_source, rscripts))
+  end_time <- Sys.time()
+  print(end_time - start_time)
+}
+
 end_time <- Sys.time()
 print(end_time - start_time)
+
 
