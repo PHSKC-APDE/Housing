f25a4cee77b134b1740184460c7bb4fca70322ea
Alastair Matheson <alastair.matheson@gmail.com>
2019-07-03 15:11:45 -0700

    Updated to use/accommodate data.table

--- R/overlap.R
+++ R/overlap.R
@@ -6,7 +6,7 @@
 #' row's elements. Its output is a new variable in the data frame. The main 
 #' purpose is for use in the \code{\link{los}} function.
 #' 
-#' @param df A data frame
+#' @param df A data frame OR data table
 #' @param ... Arguments used to identify elements to check for matches
 #' 
 #' @examples
@@ -17,13 +17,39 @@
 #' @export
 
 overlap <- function(df, ...) {
-  elements <- quos(...)
-
-  df <- df %>% mutate(
-    elements = paste0(!!!elements),
-    overlap = ifelse(is.na(lag(elements, 1)) | elements != lag(elements, 1), 0,
-                      ifelse(elements == lag(elements, 1), 1, NA))
+  if (is.data.table(df)) {
+    # Extract supplied columns to check for overlaps
+    # match.call comes from here:
+    # https://stackoverflow.com/questions/13353847/how-to-expand-an-ellipsis-argument-without-evaluating-it-in-r
+    elements <- as.character(match.call(expand.dots = F)$`...`)
+    
+    # Make variable from concatendated overlap columns
+    df[, elements := do.call(paste0, .SD), .SDcols = elements]
+    
+    # Find overlaps
+    df[, overlap := case_when(
+      is.na(lag(elements, 1)) | elements != lag(elements, 1) ~ 0,
+      elements == lag(elements, 1) ~ 1,
+      TRUE ~ NA_real_)]
+    
+    # Clean up
+    df[, elements := NULL]
+    # Return data
+    return(df)
+    
+  } else {
+    # Extract supplied columns to check for overlaps
+    elements <- quos(...)
+    df <- df %>% mutate(
+      # Make variable from concatendated overlap columns
+      elements = paste0(!!!elements),
+      # Find overlaps
+      overlap = ifelse(is.na(lag(elements, 1)) | elements != lag(elements, 1), 0,
+                       ifelse(elements == lag(elements, 1), 1, NA))
     ) %>%
-    select(-elements)
-  return(df)
+      # Clean up
+      select(-elements)
+    # Return data
+    return(df)
+  }
 }
--- processing/08_pha_analyses_prep.R
+++ processing/08_pha_analyses_prep.R
@@ -31,190 +31,204 @@ library(housing) # contains many useful functions for cleaning
 library(openxlsx) # Used to import/export Excel files
 library(lubridate) # Used to manipulate dates
 library(tidyverse) # Used to manipulate data
+library(data.table) # Used to manipulate data
 library(RJSONIO)
 library(RCurl)
 
-script <- RCurl::getURL("https://raw.githubusercontent.com/jmhernan/Housing/uw_test/processing/metadata/set_data_env.r")
-eval(parse(text = script))
-
-METADATA = RJSONIO::fromJSON("//home/ubuntu/data/metadata/metadata.json")
-
+source(file = paste0(getwd(), "/processing/metadata/set_data_env.r"))
+METADATA <- RJSONIO::fromJSON(paste0(getwd(), "/processing/metadata/metadata.json"))
 set_data_envr(METADATA,"combined")
 
 #### Bring in data ####
-pha_cleanadd_sort_dedup <- readRDS(file = paste0(
-  housing_path, pha_cleanadd_sort_dedup_fn))
-
+pha_cleanadd_sort_dedup <- readRDS(file.path(housing_path, pha_cleanadd_sort_dedup_fn))
+pha_cleanadd_sort_dedup <- setDT(pha_cleanadd_sort_dedup)
 
 
 ### Strip out some variables that no longer have meaning 
 # (e.g., act_date, act_type, household size (this will need to be remade))
 if(UW == TRUE) {
-  pha_longitudinal <- pha_cleanadd_sort_dedup %>%
-    select(
-      # Original name and SSN variables (with minor clean up)
-      ssn_new, ssn_c, lname_new, fname_new, mname_new, lnamesuf_new, dob,
-      gender_new, citizen, disability, relcode,
-      # New name, SSN, and demog variables (after matching)
-      pid, ssn_id_m6, ssn_id_m6_junk, lname_new_m6, fname_new_m6, mname_new_m6,
-      lnamesuf_new_m6, gender_new_m6, dob_m6, r_aian_new, r_asian_new, r_black_new,
-      r_nhpi_new, r_white_new, r_hisp_new, race_new, age, adult, senior,
-      # Old household info
-      mbr_num, hh_ssn_new:hh_dob, hh_id, 
-      # New household info
-      hh_ssn_id_m6:hh_id_new,
-      # MTW background info
-      # list_date, list_zip, list_homeless, housing_act,
-      # Program info
-      agency_new, major_prog, prog_type, subsidy_type, operator_type, 
-      vouch_type_final, agency_prog_concat,
-      # Old address info
-      unit_add:unit_zip,
-      # New address info
-      unit_add_new:unit_zip_new, unit_concat,
-      # Property/portfolio info
-      property_id, property_name, property_type, portfolio, portfolio_final,
-      # Unit info
-      unit_id, unit_type, unit_year, access_unit, access_req, access_rec, 
-      bed_cnt,
-      # Date info
-      admit_date, startdate, enddate,
-      # Port info
-      port_in, port_out_kcha, port_out_sha, cost_pha,
-      # Personal asset/income info
-      asset_val, asset_inc, inc_fixed, inc_vary, inc, inc_excl, inc_adj,
-      # Household asset/income info
-      hh_asset_val, hh_asset_inc, hh_asset_impute, hh_asset_inc_final,
-      hh_inc_fixed, hh_inc_vary, hh_inc, hh_inc_adj, 
-      hh_inc_tot, hh_inc_deduct, hh_inc_tot_adj,
-      # Rent info
-      rent_type:tb_rent_ceiling,
-      # Received various forms of assistance (SHA only)
-      # dropping for now
-      # Linking and ID variables
-      incasset_id, subsidy_id, vouch_num, cert_id, increment, contains("source")
-      )
-} else {
-  pha_longitudinal <- pha_cleanadd_sort_dedup %>%
-  select(
+  cols_select <- c(
     # Original name and SSN variables (with minor clean up)
-    ssn_new, ssn_c, lname_new, fname_new, mname_new, lnamesuf_new, dob,
-    gender_new, citizen, disability, relcode,
+    "ssn_new", "ssn_c", "lname_new", "fname_new", "mname_new", "lnamesuf_new", 
+    "dob", "gender_new", "citizen", "disability", "relcode",
     # New name, SSN, and demog variables (after matching)
-    pid, ssn_id_m6, ssn_id_m6_junk, lname_new_m6, fname_new_m6, mname_new_m6,
-    lnamesuf_new_m6, gender_new_m6, dob_m6, r_aian_new, r_asian_new, r_black_new,
-    r_nhpi_new, r_white_new, r_hisp_new, race_new, age, adult, senior,
+    "pid", "ssn_id_m6", "ssn_id_m6_junk", "lname_new_m6", "fname_new_m6", 
+    "mname_new_m6", "lnamesuf_new_m6", "gender_new_m6", 
+    "dob_m6", "age", "adult", "senior",
+    "r_aian_new", "r_asian_new", "r_black_new", "r_nhpi_new", 
+    "r_white_new", "r_hisp_new", "race_new",
     # Old household info
-    mbr_num, hh_ssn_new:hh_dob, hh_id, 
+    "mbr_num", "hh_ssn_new", "hh_ssn_c", "hh_lname", "hh_fname", "hh_mname",
+    "hh_lnamesuf", "hh_dob", "hh_id", 
     # New household info
-    hh_ssn_id_m6:hh_id_new,
+    "hh_ssn_id_m6", "hh_ssn_id_m6_junk", "hh_lname_m6", "hh_lnamesuf_m6", 
+    "hh_fname_m6", "hh_mname_m6", "hh_dob_m6", "hh_id_new",
     # MTW background info
-    list_date, list_zip, list_homeless, housing_act,
+    # list_date, list_zip, list_homeless, housing_act,
     # Program info
-    agency_new, major_prog, prog_type, subsidy_type, operator_type, 
-    vouch_type_final, agency_prog_concat,
+    "agency_new", "major_prog", "prog_type", "subsidy_type", "operator_type", 
+    "vouch_type_final", "agency_prog_concat",
     # Old address info
-    unit_add:unit_zip,
+    "unit_add", "unit_apt", "unit_apt2", "unit_city", "unit_state", "unit_zip",
     # New address info
-    unit_add_new:unit_zip_new, unit_concat, check_esri, check_google, 
-    check_opencage, geocode_source, zip_centroid, add_geocoded, zip_geocoded, 
-    lon, lat,
+    "unit_add_new", "unit_apt_new", "unit_city_new", "unit_state_new", 
+    "unit_zip_new", "unit_concat",
     # Property/portfolio info
-    property_id, property_name, property_type, portfolio, portfolio_final,
+    "property_id", "property_name", "property_type", "portfolio", "portfolio_final",
     # Unit info
-    unit_id, unit_type, unit_year, access_unit, access_req, access_rec, 
-    bed_cnt, move_in_date,
+    "unit_id", "unit_type", "unit_year", "access_unit", "access_req", "access_rec", 
+    "bed_cnt",
     # Date info
-    admit_date, startdate, enddate,
+    "admit_date", "startdate", "enddate",
     # Port info
-    port_in, port_out_kcha, port_out_sha, cost_pha,
+    "port_in", "port_out_kcha", "port_out_sha", "cost_pha",
     # Personal asset/income info
-    asset_val, asset_inc, inc_fixed, inc_vary, inc, inc_excl, inc_adj,
+    "asset_val", "asset_inc", "inc_fixed", "inc_vary", "inc", "inc_excl", "inc_adj",
     # Household asset/income info
-    hh_asset_val, hh_asset_inc, hh_asset_impute, hh_asset_inc_final,
-    hh_inc_fixed, hh_inc_vary, hh_inc, hh_inc_adj, 
-    hh_inc_tot, hh_inc_deduct, hh_inc_tot_adj,
+    "hh_asset_val", "hh_asset_inc", "hh_asset_impute", "hh_asset_inc_final",
+    "hh_inc_fixed", "hh_inc_vary", "hh_inc", "hh_inc_adj", 
+    "hh_inc_tot", "hh_inc_deduct", "hh_inc_tot_adj",
     # Rent info
-    rent_type:tb_rent_ceiling,
+    "rent_type", "rent_tenant", "rent_mixfam", "ph_util_allow", "ph_rent_ceiling",
+    "subs_type", "bdrm_voucher", "cost_month", "rent_owner", "tb_util_allow", 
+    "rent_gross", "rent_subs", "rent_tenant_owner", 
+    "rent_mixfam_owner", "tb_rent_ceiling",
     # Received various forms of assistance (SHA only)
     # dropping for now
     # Linking and ID variables
-    incasset_id, subsidy_id, vouch_num, cert_id, increment, contains("source")
-    )
+    "incasset_id", "subsidy_id", "vouch_num", "cert_id", "increment", 
+    "sha_source", "kcha_source", "eop_source")
+} else {
+  cols_select <- c(
+    # Original name and SSN variables (with minor clean up)
+    "ssn_new", "ssn_c", "lname_new", "fname_new", "mname_new", "lnamesuf_new", 
+    "dob", "gender_new", "citizen", "disability", "relcode",
+    # New name, SSN, and demog variables (after matching)
+    "pid", "ssn_id_m6", "ssn_id_m6_junk", "lname_new_m6", "fname_new_m6", 
+    "mname_new_m6", "lnamesuf_new_m6", "gender_new_m6", 
+    "dob_m6", "age", "adult", "senior",
+    "r_aian_new", "r_asian_new", "r_black_new", "r_nhpi_new", 
+    "r_white_new", "r_hisp_new", "race_new",
+    # Old household info
+    "mbr_num", "hh_ssn_new", "hh_ssn_c", "hh_lname", "hh_fname", "hh_mname",
+    "hh_lnamesuf", "hh_dob", "hh_id", 
+    # New household info
+    "hh_ssn_id_m6", "hh_ssn_id_m6_junk", "hh_lname_m6", "hh_lnamesuf_m6", 
+    "hh_fname_m6", "hh_mname_m6", "hh_dob_m6", "hh_id_new",
+    # MTW background info
+    "list_date", "list_zip", "list_homeless", "housing_act",
+    # Program info
+    "agency_new", "major_prog", "prog_type", "subsidy_type", "operator_type", 
+    "vouch_type_final", "agency_prog_concat",
+    # Old address info
+    "unit_add", "unit_apt", "unit_apt2", "unit_city", "unit_state", "unit_zip",
+    # New address info
+    "unit_add_new", "unit_apt_new", "unit_city_new", "unit_state_new", 
+    "unit_zip_new", "unit_concat",
+    # Property/portfolio info
+    "property_id", "property_name", "property_type", "portfolio", "portfolio_final",
+    # Unit info
+    "unit_id", "unit_type", "unit_year", "access_unit", "access_req", "access_rec", 
+    "bed_cnt",
+    # Date info
+    "admit_date", "startdate", "enddate",
+    # Port info
+    "port_in", "port_out_kcha", "port_out_sha", "cost_pha",
+    # Personal asset/income info
+    "asset_val", "asset_inc", "inc_fixed", "inc_vary", "inc", "inc_excl", "inc_adj",
+    # Household asset/income info
+    "hh_asset_val", "hh_asset_inc", "hh_asset_impute", "hh_asset_inc_final",
+    "hh_inc_fixed", "hh_inc_vary", "hh_inc", "hh_inc_adj", 
+    "hh_inc_tot", "hh_inc_deduct", "hh_inc_tot_adj",
+    # Rent info
+    "rent_type", "rent_tenant", "rent_mixfam", "ph_util_allow", "ph_rent_ceiling",
+    "subs_type", "bdrm_voucher", "cost_month", "rent_owner", "tb_util_allow", 
+    "rent_gross", "rent_subs", "rent_tenant_owner", 
+    "rent_mixfam_owner", "tb_rent_ceiling",
+    # Received various forms of assistance (SHA only)
+    # dropping for now
+    # Linking and ID variables
+    "incasset_id", "subsidy_id", "vouch_num", "cert_id", "increment", 
+    "sha_source", "kcha_source", "eop_source")
 }
 
+pha_longitudinal <- pha_cleanadd_sort_dedup[, ..cols_select]
+
+
 ### Set up time in housing for each row and note when there was a gap in coverage
-pha_longitudinal <- pha_longitudinal %>% 
-  mutate(cov_time = interval(start = startdate, end = enddate) / ddays(1) + 1,
-         gap = case_when(
-           is.na(lag(pid, 1)) | pid != lag(pid, 1) ~ 0,
-           startdate - lag(enddate, 1) > 62 ~ 1,
-           TRUE ~ NA_real_
-         )) %>%
-  # Find the number of unique periods a person was in housing
-  group_by(pid, gap) %>%
-  mutate(period = row_number() * gap + 1) %>% 
-  ungroup() %>%
-  # Fill in missing data using previous non-missing value
-  tidyr::fill(., period) %>%
-  select(-gap)
+pha_longitudinal <- pha_longitudinal[order(pid, startdate, enddate)]
+pha_longitudinal[, ':=' (
+  cov_time = interval(start = startdate, end = enddate) / ddays(1) + 1,
+  gap = case_when(is.na(lag(pid, 1)) | pid != lag(pid, 1) ~ 0,
+                  startdate - lag(enddate, 1) > 62 ~ 1,
+                  TRUE ~ NA_real_))]
+# Find the number of unique periods a person was in housing
+pha_longitudinal[, period := rowid(pid, gap) * gap + 1]
+
+# Fill in missing data using previous non-missing value
+# Use new fill NA function in data.table (requires 1.12.3 or higher)
+setnafill(pha_longitudinal, "locf", cols = "period")
+pha_longitudinal[, gap := NULL]
 
 
 ### Flag date to use when calculating length of stay
 pha_longitudinal <- los(pha_longitudinal)
 
 # Set up length of stay using the end date
-pha_longitudinal <- pha_longitudinal %>%
-  mutate(
-    time_housing = round(interval(start = start_housing, end = enddate) / years(1), 1),
-    time_pha = round(interval(start = start_pha, end = enddate) / years(1), 1),
-    time_prog = round(interval(start = start_prog, end = enddate) / years(1), 1)
-  )
+pha_longitudinal[, ':=' (
+  time_housing = round(interval(start = start_housing, end = enddate) / years(1), 1),
+  time_pha = round(interval(start = start_pha, end = enddate) / years(1), 1),
+  time_prog = round(interval(start = start_prog, end = enddate) / years(1), 1)
+)]
+
 
 ### Age
 # Age at each date
-pha_longitudinal <- pha_longitudinal %>%
-  mutate(age12 = round(interval(start = dob_m6, end = ymd(20121231)) / years(1), 1),
-         age13 = round(interval(start = dob_m6, end = ymd(20131231)) / years(1), 1),
-         age14 = round(interval(start = dob_m6, end = ymd(20141231)) / years(1), 1),
-         age15 = round(interval(start = dob_m6, end = ymd(20151231)) / years(1), 1),
-         age16 = round(interval(start = dob_m6, end = ymd(20161231)) / years(1), 1),
-         age17 = round(interval(start = dob_m6, end = ymd(20171231)) / years(1), 1)
-  )
+pha_longitudinal[, ':=' (
+  age12 = round(interval(start = dob_m6, end = ymd(20121231)) / years(1), 1),
+  age13 = round(interval(start = dob_m6, end = ymd(20131231)) / years(1), 1),
+  age14 = round(interval(start = dob_m6, end = ymd(20141231)) / years(1), 1),
+  age15 = round(interval(start = dob_m6, end = ymd(20151231)) / years(1), 1),
+  age16 = round(interval(start = dob_m6, end = ymd(20161231)) / years(1), 1),
+  age17 = round(interval(start = dob_m6, end = ymd(20171231)) / years(1), 1)
+)]
+
 
 # Single age grouping
 # Note: this is only valid for the person at the time of the start date
 # Need to recalculate for each time period being analyzed
-pha_longitudinal <- pha_longitudinal %>%
-  mutate(agegrp = case_when(
-    adult == 0 & !is.na(adult) ~ "Youth",
-    adult == 1 & senior == 0 & !is.na(adult) ~ "Working age",
-    adult == 1 & senior == 1 & !is.na(adult) ~ "Senior",
-    TRUE ~ NA_character_
-    ))
+pha_longitudinal[, agegrp := case_when(
+  adult == 0 & !is.na(adult) ~ "Youth",
+  adult == 1 & senior == 0 & !is.na(adult) ~ "Working age",
+  adult == 1 & senior == 1 & !is.na(adult) ~ "Senior",
+  TRUE ~ NA_character_)]
+
 
 ### Recode gender and disability
-pha_longitudinal <- pha_longitudinal %>%
-  mutate(gender2 = car::recode(gender_new_m6, "'1' = 'Female'; '2' = 'Male'; else = NA"),
-         disability2 = car::recode(disability, "'1' = 'Disabled'; '0' = 'Not disabled'; else = NA"))
+pha_longitudinal[, ':=' (
+  gender2 = recode(gender_new_m6, "1" = "Female", "2" = "Male", .default = NA_character_),
+  disability2 = recode(disability, "1" = "Disabled", "0" = "Not disabled", .default = NA_character_)
+)]
 
 
 ### ZIPs to restrict to KC and surrounds (helps make maps that drop far-flung ports)
 zips <- read.csv(text = RCurl::getURL("https://raw.githubusercontent.com/PHSKC-APDE/reference-data/master/Spatial%20data/KC%20ZIPs.csv"), 
                  header = TRUE, stringsAsFactors = FALSE)
 names(zips) <- tolower(names(zips))
-zips <- zips %>% select(zipcode) %>% 
-  mutate(kc_area = 1)
+zips <- zips %>% select(zipcode) %>% mutate(zipcode = as.character(zipcode), kc_area = 1)
+zips <- setDT(zips)
 
-pha_longitudinal <- pha_longitudinal %>%
-  mutate(unit_zip_new = as.numeric(unit_zip_new)) %>%
-  left_join(., zips, by = c("unit_zip_new" = "zipcode"))
+
+pha_longitudinal <- merge(pha_longitudinal, zips, 
+                          by.x = c("unit_zip_new"), by.y = c("zipcode"),
+                          all.x = TRUE, sort = F)
+
 rm(zips)
 
 
 
 #### Save point ####
-saveRDS(pha_longitudinal, file = paste0(housing_path, pha_longitudinal_fn))
+saveRDS(pha_longitudinal, file = file.path(housing_path, pha_longitudinal_fn))
 
 ### Clean up remaining data frames
 rm(pha_cleanadd_sort_dedup)
