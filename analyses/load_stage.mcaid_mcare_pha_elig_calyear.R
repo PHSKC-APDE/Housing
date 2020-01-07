c6c95ef30f555078af37e6dc3bf7c06d9ce5a76b
Alastair Matheson <alastair.matheson@gmail.com>
2020-01-07 12:48:55 -0800

    Updated to reflect additional variables in elig tables, now runs to 2018

--- analyses/load_stage.mcaid_mcare_pha_elig_calyear.R
+++ analyses/load_stage.mcaid_mcare_pha_elig_calyear.R
@@ -16,7 +16,7 @@ library(housing) # contains many useful functions for analyzing housing/Medicaid
 library(lubridate) # Used to manipulate dates
 library(tidyverse) # Used to manipulate data
 library(data.table) # Used to manipulate data
-library(medicaid) # Used to aggregate data
+library(claims) # Used to aggregate data
 
 
 ##### Connect to the SQL servers #####
@@ -64,7 +64,7 @@ mcaid_mcare_pha_elig_timevar <- mcaid_mcare_pha_elig_timevar %>%
 
 #### RUN ALLOCATION FOR EACH CALENDAR YEAR ####
 # Set up calendar years
-years <- seq(2012, 2016)
+years <- seq(2012, 2018)
 
 allocated <- bind_rows(lapply(seq_along(years), function(x) {
   year <- allocate(df = mcaid_mcare_pha_elig_timevar, 
@@ -77,14 +77,14 @@ allocated <- bind_rows(lapply(seq_along(years), function(x) {
                    apde_dual, full_benefit, pha_subsidy, pha_voucher, 
                    pha_operator, pha_portfolio, geo_zip) %>%
     mutate(year = years[x]) %>%
-    select(year, id_apde:pha, enroll_type, apde_dual, geo_kc, part_a:pha_portfolio, 
-           geo_zip:geo_school_code, pt, pt_tot)
+    select(-last_run, -geo_add1, -geo_add2, -geo_city, -geo_state, -pt_allocate)
 }))
-
 
 #### MAKE PT AND POP_EVER FIELDS ####
 # Want to keep a row for any combination of groups vars that appeared
 pt_rows <- bind_rows(lapply(seq_along(years), function(x) {
+  
+  message(glue("Working on {years[x]}"))
   
   output <- mcaid_mcare_pha_elig_timevar %>%
     mutate(
@@ -100,7 +100,7 @@ pt_rows <- bind_rows(lapply(seq_along(years), function(x) {
              apde_dual, full_benefit, pha_subsidy, pha_voucher, 
              pha_operator, pha_portfolio, geo_zip) %>%
     summarise(pop_ever = n_distinct(id_apde),
-              pt_days = sum(overlap_amount)) %>%
+              pt = sum(overlap_amount)) %>%
     ungroup() %>%
     mutate(year = years[x])
 
@@ -108,15 +108,12 @@ pt_rows <- bind_rows(lapply(seq_along(years), function(x) {
   return(output)
 }))
 
-
-
-
 #### JOIN TO ELIG_DEMO AND ADD CALCULATED FIELDS ####
 mcaid_mcare_pha_elig_calyear <- left_join(pt_rows, 
                   allocated, by = c("year", "id_apde", "enroll_type", "pha_agency", 
                                     "apde_dual", "full_benefit", "pha_subsidy", 
                                     "pha_voucher", "pha_operator", "pha_portfolio", "geo_zip")) %>%
-  left_join(., select(mcaid_mcare_pha_elig_demo, -apde_dual, -last_run), 
+  left_join(., select(mcaid_mcare_pha_elig_demo, -mcaid_mcare_pha, -apde_dual, -last_run), 
             by = "id_apde")
 
 
@@ -128,8 +125,7 @@ mcaid_mcare_pha_elig_calyear <- mcaid_mcare_pha_elig_calyear %>%
            age_yr < 18 ~ "<18",
            between(age_yr, 18, 24.99) ~ "18-24",
            between(age_yr, 25, 44.99) ~ "24-44",
-           between(age_yr, 45, 61.99) ~ "45-61",
-           between(age_yr, 62, 64.99) ~ "62-64",
+           between(age_yr, 45, 64.99) ~ "45-64",
            age_yr >= 65 ~ "65+",
            is.na(age_yr) ~ NA_character_),
          age_wc = case_when(between(age_yr, 0, 6.99) ~ "Children aged 0-6", 
@@ -146,43 +142,25 @@ mcaid_mcare_pha_elig_calyear <- mcaid_mcare_pha_elig_calyear %>%
   )
 
 
-### Rearrange fields
-mcaid_mcare_pha_elig_calyear <- mcaid_mcare_pha_elig_calyear %>%
-  select(year, id_apde, mcaid, mcare, pha, mcaid_mcare_pha, enroll_type, 
-         pha_agency, apde_dual, part_a, part_b, part_c, partial, buy_in, tpl, 
-         bsp_group_name, cov_type, mco_id, full_benefit,
-         dob, death_dt, age_yr, adult, senior, agegrp, age_wc,
-         gender_me:gender_male, race_me:race_unk, 
-         start_housing, time_housing_yr, time_housing, 
-         pha_subsidy, pha_voucher, pha_operator, pha_portfolio, geo_kc, geo_zip, 
-         geo_zip_centroid, geo_street_centroid, geo_county_code, geo_tract_code,
-         geo_hra_code, geo_school_code, pt_days, pt_tot, pop, pop_ever,
-         last_run)
-
-
-#### TEMP UNTIL ELIG TABLES ARE UPDATED ####
-# Just go through to 2016 with joint data
-mcaid_mcare_pha_elig_calyear <- mcaid_mcare_pha_elig_calyear %>% filter(year < 2017)
-
-
-#### Find out why so many are missing addresses ####
-mcaid_mcare_pha_elig_timevar %>% filter(pha == 1) %>% summarise(count = n()) # 620391
-mcaid_mcare_pha_elig_timevar %>% filter(pha == 1 & is.na(geo_add1)) %>% summarise(count = n()) # 5292
-
-
 #### WRITE DATA TO SQL SERVER ####
-table_config_stage <- yaml::yaml.load_file("C:/Users/mathesal/OneDrive - King County/github/Housing/analyses/load.stage_mcaid_mcare_pha_elig_calyear.yaml")
+table_config_stage <- yaml::yaml.load(RCurl::getURL(
+  "https://raw.githubusercontent.com/PHSKC-APDE/Housing/master/analyses/load.stage_mcaid_mcare_pha_elig_calyear.yaml"))
 source("https://raw.githubusercontent.com/PHSKC-APDE/claims_data/master/claims_db/db_loader/scripts_general/add_index.R")
 
 
-### Temp: remove index
-dbGetQuery(db_apde51, 'DROP INDEX "idx_ccs_stage_mcaid_mcare_pha_elig_calyear" ON stage.mcaid_mcare_pha_elig_calyear')
+# Ensure columns are in the correct order
+data.table::setcolorder(mcaid_mcare_pha_elig_calyear, names(table_config_stage$vars))
 
-
+# Load to SQL
 DBI::dbWriteTable(db_apde51,
                   name = DBI::Id(schema = "stage", table = "mcaid_mcare_pha_elig_calyear"),
                   value = mcaid_mcare_pha_elig_calyear,
-                  append = F, overwrite = T)
+                  append = F, overwrite = T,
+                  field.types = unlist(table_config_stage$vars))
+
+
+### Add index
+add_index_f(db_apde51, table_config = table_config_stage, drop_index = T)
 
 
 #### QA TABLE AND MOVE TO FINAL ####
@@ -190,11 +168,7 @@ DBI::dbWriteTable(db_apde51,
 
 
 
-#### ADD INDEX TO SQL ####
-# For now, adding it to stage
 
-
-add_index_f(db_apde51, table_config = table_config_stage, drop_index = T)
 
 
 #### CLEAN UP ####
